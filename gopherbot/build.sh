5daaafc7d1e53b7724613aaedc445256304b5ea6
David Parsley <parsley@linuxjedi.org>
2020-08-03 11:26:49 -0400

    Add robot for aws cluster

---
+++ gopherbot/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+podman build -f Containerfile -t lnxjedi/gopherbot-aws-k8s -t 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s .
---
+++ gopherbot/push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+podman push 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s
