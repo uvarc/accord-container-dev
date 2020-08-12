33e06723d6fa2ba4d8352fd0d662c979efb72870
David Parsley <parsley@linuxjedi.org>
2020-08-12 09:38:35 -0400

    Add latest to tag

--- gopherbot/build.sh
+++ gopherbot/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman build -f Containerfile -t lnxjedi/gopherbot-aws-k8s -t 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s .
+podman build -f Containerfile -t lnxjedi/gopherbot-aws-k8s:latest -t 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s:latest .
--- gopherbot/push.sh
+++ gopherbot/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman push 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s
+podman push 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s:latest
