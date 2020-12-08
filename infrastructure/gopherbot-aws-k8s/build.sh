25d8646f2e34db60518ffa7c48382c6dd84d777d
David Parsley <parsley@linuxjedi.org>
2020-12-08 08:14:09 -0500

    Remove old infrastructure

--- infrastructure/gopherbot-aws-k8s/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-podman build -f Containerfile -t lnxjedi/gopherbot-aws-k8s:latest -t 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s:latest .
--- infrastructure/gopherbot-aws-k8s/push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-podman push 474683445819.dkr.ecr.us-east-1.amazonaws.com/accord/gopherbot-aws-k8s:latest
