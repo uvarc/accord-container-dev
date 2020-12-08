74e54f4b217ad26dce564b84341723fc43953f2a
David Parsley <parsley@linuxjedi.org>
2020-12-08 11:53:23 -0500

    Build trigger script

---
+++ quay-build-trigger.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+# quaytrigger.sh - trigger a container build on quay.io
+# NOTE: this is NOT general-purpose, but hard-coded to use
+# with github.com/accord/XXX
+
+if [ ! -e ".env" ]
+then
+    echo "Missing '.env' file."
+    exit 1
+fi
+
+if [ ! "$GITHUB_TOKEN" ]
+then
+    echo "GITHUB_TOKEN not set in environment"
+    exit 1
+fi
+
+if [ ! "$1" ]
+then
+    echo "Missing argument: <container-name>"
+    exit 1
+fi
+
+trap_handler()
+{
+    ERRLINE="$1"
+    ERRVAL="$2"
+    echo "line ${ERRLINE} exit status: ${ERRVAL}" >&2
+    # The script should usually exit on error
+    exit $ERRVAL
+}
+trap 'trap_handler ${LINENO} $?' ERR
+
+source ".env"
+
+REPO=$1
+WEBHOOK_ENV=$(echo $REPO | tr - _)
+WEBHOOK=${!WEBHOOK_ENV}
+
+if [ ! "$WEBHOOK" ]
+then
+    echo "Webhook not found for $REPO"
+    exit 1
+fi
+
+COMMIT=$(gh api /repos/uvarc/accord-container-dev/branches | jq -r '.[] | select(.name=="master") | .commit.sha')
+
+echo "Building master/commit: $COMMIT"
+
+echo "{ \"commit\": \"$COMMIT\", \"ref\": \"refs/heads/master\", \"default_branch\": \"master\" }" | curl -X POST -H "Content-Type: application/json" -d @- $WEBHOOK
+
+echo
+exit 0
